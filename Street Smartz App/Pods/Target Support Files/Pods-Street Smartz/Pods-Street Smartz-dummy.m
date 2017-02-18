13c753b4aa7839c3dd30a6a5b91d8273e79d744e
grawson <grawson2@jhu.edu>
2017-02-18 05:29:06 -0500

    working camera

---
+++ Street Smartz App/Pods/Target Support Files/Pods-Street Smartz/Pods-Street Smartz-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Street_Smartz : NSObject
+@end
+@implementation PodsDummy_Pods_Street_Smartz
+@end
--- Street Smartz App/Street Smartz/CameraViewController.swift
+++ Street Smartz App/Street Smartz/CameraViewController.swift
@@ -10,32 +10,65 @@ import UIKit
 import AVFoundation
 
 class CameraViewController: UIViewController {
+    
+    var captureSession = AVCaptureSession()
+    var sessionOutput = AVCapturePhotoOutput()
+    var previewLayer = AVCaptureVideoPreviewLayer()
 
     // MARK - Outlets
     /******************************************************/
     
-    @IBOutlet weak var previewImage: UIView!
+    @IBOutlet weak var captureButton: UIButton!
+    @IBOutlet weak var cameraView: UIView!
     
-    
-    // MARK - Actions
-    /******************************************************/
-   
     @IBAction func capturePressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let deviceSession = AVCaptureDeviceDiscoverySession(
+            deviceTypes: [.builtInDuoCamera, .builtInTelephotoCamera, .builtInWideAngleCamera],
+            mediaType: AVMediaTypeVideo, position: .unspecified)
+        
+        for device in (deviceSession?.devices)! {
+            if device.position == AVCaptureDevicePosition.front {
+                do {
+                    
+                    let input = try AVCaptureDeviceInput(device: device)
+                    if captureSession.canAddInput(input) {
+                        captureSession.addInput(input)
+                        if captureSession.canAddOutput(sessionOutput) {
+                            captureSession.addOutput(sessionOutput)
+                            
+                            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                            previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                            previewLayer.connection.videoOrientation = .portrait
+                            cameraView.layer.addSublayer(previewLayer)
+                            cameraView.addSubview(captureButton)
+                            
+                            previewLayer.position = CGPoint(x: cameraView.frame.width/2, y: cameraView.frame.height/2)
+                            previewLayer.bounds = cameraView.frame
+                            
+                            captureSession.startRunning()
+                            
+                        }
+                    }
+                    
+                } catch let avError {
+                    print(avError)
+                }
+            }
+            
+        }
         
     }
     
-    // MARK - Variables
-    /******************************************************/
-   
-    var captureSession: AVCaptureSession?
-    var stillImageOutput: AVCaptureStillImageOutput?
-    var previewLayer: AVCaptureVideoPreviewLayer?
-   
-    
-    // MARK - Life Cycle
-    /******************************************************/
-    
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
     
 }
