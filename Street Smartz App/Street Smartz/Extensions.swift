1ad11643c0501fda63334a537f8927a30733ad9a
grawson <grawson2@jhu.edu>
2017-02-18 15:56:09 -0500

    alamofire uploading

---
+++ Street Smartz App/Street Smartz/Extensions.swift
@@ -0,0 +1,27 @@
+//
+//  Extensions.swift
+//  Street Smartz
+//
+//  Created by Gavi Rawson on 2/18/17.
+//  Copyright © 2017 Graws Inc. All rights reserved.
+//
+
+import Foundation
+
+//extension String {
+//    //: ### Base64 encoding a string
+//    func base64Encoded() -> String? {
+//        if let data = self.data(using: .utf8) {
+//            return data.base64EncodedString()
+//        }
+//        return nil
+//    }
+//    
+//    //: ### Base64 decoding a string
+//    func base64Decoded() -> String? {
+//        if let data = Data(base64Encoded: self) {
+//            return String(data: data, encoding: .utf8)
+//        }
+//        return nil
+//    }
+//}
