a6d95f93e3028e470bef9113a1dda4e80a59870e
Alex Ahn <alexahn917@gmail.com>
2017-02-19 07:21:40 -0500

    3 Modules: Object, Face, Walk completed

--- answer.py
+++ answer.py
@@ -15,72 +15,62 @@ def main():
 def ans(mode, image):
     cv_img = cv2.imread(image)
     cv2.imwrite("temp.jpg", cv_img)
-    return_answer = -1
-    is_good = 1
-
-    # crosswalk lights classifiers (cascades)
-    red_object_cascade = cv2.CascadeClassifier('cascade/crosswalk_redlight.xml')
-    white_object_cascade = cv2.CascadeClassifier('cascade/crosswalk_greenlight.xml')        
+    with io.open("temp.jpg", 'rb') as image_file:
+        temp_img = image_file.read()    
 
     # classify objects
     if mode == 'object':
-        vision_client = vision.Client()
-        with io.open("temp.jpg", 'rb') as image_file:
-            content = image_file.read()
-        image = vision_client.image(content=content)
-        labels = image.detect_labels()
-        print('Labels:')
-        for label in labels:
-            print("object label: '%s' with the score of %f" % (label.description, label.score))
-            return labels
+        return(detect_objects(temp_img))
 
     # make face detection
-    if mode == 'face':
-        emotions = emotion_detection(open(image, "rb"))
-        # print emotions
-        print("There are in total %d many faces with:" %(len(emotions)))
-        emotion_counts = {}
-        for emotion in emotions:
-            emotion_counts.update({emotion:emotion_counts.get(emotion,0)+1})
-        for emot, counts in emotion_counts.iteritems():
-            print("%d %s faces" %(counts,emot))
-        return(emotions)
+    elif mode == 'face':
+        return(detect_faces(image))
 
     # mode == 'walk'
-    else: 
-        reds = red_object_cascade.detectMultiScale(cv_img, 1.3, 5)
-        whites = white_object_cascade.detectMultiScale(cv_img, 1.3, 5)
-        # no detection
-        if len(reds) + len(whites) is 0:
-            print("No detection")
-            return(-1)
+    elif mode == 'walk':
+        return(may_walk(cv_img))
 
-        # get score for "Lighting, Signage, Display Device, Electronic Signage, Flat Panel Display"
-#        for img in objects:
-#            JSON_obj = google_function(img)
-#            scores.append(get_obj_score(JSON_obj))
-
-        for sign in [reds, whites]:
-            for (x,y,w,h) in sign:
-                cv2.rectangle(cv_img,(x,y),(x+w,y+h),(255,0,0),2)
-                box = cv_img[y:y+h, x:x+w]
-                cv2.imshow('cv_img',cv_img)
-                cv2.waitKey(0)
-                if white_over_red(box) is False:
-                    print("Not good to go.")
-                    return(-1)
-        print("Good to go.")
-        return(1)
-
-def detect_labels(path):
+def detect_objects(content):
     vision_client = vision.Client()
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
     image = vision_client.image(content=content)
     labels = image.detect_labels()
     print('Labels:')
     for label in labels:
-        print(label.description)
+        print("object label: '%s' with the score of %f" % (label.description, label.score))
+        return labels
+
+def detect_faces(image):
+    emotions = emotion_detection(open(image, "rb"))
+    # print emotions
+    print("There are in total %d many faces with:" %(len(emotions)))
+    emotion_counts = {}
+    for emotion in emotions:
+        emotion_counts.update({emotion:emotion_counts.get(emotion,0)+1})
+    for emot, counts in emotion_counts.iteritems():
+        print("%d %s faces" %(counts,emot))
+    return(emotions)
+
+def may_walk(cv_img):
+    # crosswalk lights classifiers (cascades)
+    red_object_cascade = cv2.CascadeClassifier('cascade/crosswalk_redlight.xml')
+    white_object_cascade = cv2.CascadeClassifier('cascade/crosswalk_greenlight.xml')
+    reds = red_object_cascade.detectMultiScale(cv_img, 1.3, 5)
+    whites = white_object_cascade.detectMultiScale(cv_img, 1.3, 5)
+    # no detection
+    if len(reds) + len(whites) is 0:
+        print("No detection")
+        return(-1)
+    for sign in [reds, whites]:
+        for (x,y,w,h) in sign:
+            cv2.rectangle(cv_img,(x,y),(x+w,y+h),(255,0,0),2)
+            box = cv_img[y:y+h, x:x+w]
+            cv2.imshow('cv_img',cv_img)
+            cv2.waitKey(0)
+            if white_over_red(box) is False:
+                print("Not good to go.")
+                return(-1)
+    print("Good to go.")
+    return(1)
 
 def get_obj_score(JSON_obj):
     scores = 0
---
+++ cmd.py
@@ -0,0 +1,4 @@
+python answer.py object images/ppl1.jpg
+python answer.py face images/ppl1.jpg
+python answer.py walk images/green1.jpg
+python answer.py walk images/red1.jpg
